ee6e7430f28ed88ec8a9df94a489d30524b8c40b
nahsra58 <nahsra58@gmail.com>
2019-11-24 21:30:30 -0800

    test

--- javascript-tests/object-choices.js
+++ javascript-tests/object-choices.js
@@ -1,3 +1,41 @@
 window.onload = () => {
+
+
+    const simpleObject = {
+
+        firstName: "James",
+        lastName: "Watson",
+
+        sayName: function () {
+
+            const fullName= this.firstName + this.lastName;
+
+            console.log(fullName);
+        }
+    };
+
     const simpleObjectDisplay = document.getElementById('simpleObjectDisplay')
+    const functionObjectDisplay = document.getElementById('simpleObjectDisplay')
+    const customClassDisplay = document.getElementById('customClassDisplay')
+
+    simpleObjectAction.onclick = function() {
+        simpleObject.sayName();
+        simpleObjectDisplay.textContent=fullName;
+    }
+
+    simpleObject.dynamicMethod = () => {
+
+        return("Dynamic Method");
+
+    }
+
+    function FunctionObject() {
+
+        FunctionObject.prototype.sayName = function () {
+
+            return("Function Object");
+        }
+    }
+
+    
 }
