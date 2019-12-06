7c7490c1d49559ab3b9a177dfa098ba129067163
nahsra58 <nahsra58@gmail.com>
2019-12-06 15:59:33 -0800

    for loops test

--- for-loops/index.js
+++ for-loops/index.js
@@ -1,11 +1,19 @@
 window.onload = () => {
-    const listButton = document.getElementById('runForLoop');
-
+    const listButton = document.getElementById('runForLoopAction');
+    const elfCode = {
+        appendToList: (list, value) => {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(value));
+            list.appendChild(li);
+        }
+    };
     listButton.onclick = () => {
-        const myList = document.getElementById('myListt');
+        const myList = document.getElementById('myList');
             // WRAP THE NEXT LINE IN A for-loop that iterates over a variable named i.
             // The loop should iterate 5 times (count from zero to five).
+            for(i=0; i<=5;i++) {
             elfCode.appendToList(myList, i);
+            };
             // END LOOP
     }
 }
